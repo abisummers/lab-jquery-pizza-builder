8938b328886e66585d2432d394fab14ad709d3c6
abisummers <hello@abisummers.com>
2018-08-15 18:29:09 +0200

    pepper, mushroom and pepperonni toggle

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,16 @@
 // Write your Pizza Builder JavaScript in this file.
+$(".sauce").removeClass("sauce-white");
+$(".btn-sauce").click(function() {
+  var sauces = $(".sauce section").addClass("sauce-white");
+
+  $(".sauce-white").toggle();
+});
+$(".btn-green-peppers").click(function() {
+  $(".green-pepper").toggle();
+});
+$(".btn-mushrooms").click(function() {
+  $(".mushroom").toggle();
+});
+$(".btn-pepperonni").click(function() {
+  $(".pep").toggle();
+});
